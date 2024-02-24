81d72346ce27cb4a63ef106898a124d7e77aa56c
FlaskR <adnan23khan23@gmail.com>
2024-02-24 16:35:09 +0530

    temporarily commented code

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -117,7 +117,7 @@ export default async function NewNoteRoute() {
         </CardFooter>
       </form>
 
-      {jsonData && (
+      {/* {jsonData && (
         <Card>
           <CardHeader>
             <CardTitle>Fetched Data</CardTitle>
@@ -128,7 +128,7 @@ export default async function NewNoteRoute() {
             <div dangerouslySetInnerHTML={{ __html: jsonData["content"] }} /> 
           </CardContent>
         </Card>
-      )}
+      )} */}
     </Card>
   );
 }
